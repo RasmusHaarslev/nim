23ffd2be0afe642a1e83883ebf6aa6b959d982fd
rhaarslev <rha@it-minds.dk>
2017-01-02 14:32:01 +0100

    Adds function to find next AI move

--- Ai.fsx
+++ Ai.fsx
@@ -1,5 +1,39 @@
-let rec findm heap =
-    match heap with
-    | [] -> 0
-    | [x] -> x
-    | x::xs -> x ^^^ findm xs
+module Ai
+    #load "AsyncEventQueue.fsx"
+    open AsyncEventQueue
+
+    // 
+    let rec findm heap =
+        match heap with
+        | [] -> 0
+        | x::xs -> x ^^^ findm xs
+
+    let findMove heap m =
+        let rec innerFoo heap m idx =
+            match heap with
+            | [] -> failwith "Couldn't find k where ak < m"
+            | x::xs ->
+                let ak = x ^^^ m
+                in
+                    if ak < x then (idx, m - ak)
+                    else innerFoo xs m (idx+1)
+        in
+            innerFoo heap m 0
+
+    let maxIndexBy list =
+        list
+        |> List.mapi (fun i x -> i,  x)
+        |> List.maxBy snd
+        |> fst
+
+    let aiMove heap =
+        let m = findm heap
+
+        if m = 0 then
+            let largestHeapIdx = maxIndexBy heap
+            in
+                printf "Apply move (%d, 1)" largestHeapIdx
+        else
+            let (heapIdx, removeCount) = findMove heap m
+            in
+                printf "Apply move (%d, %d)" heapIdx removeCount
