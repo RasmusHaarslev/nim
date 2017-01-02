8b150e802bf8eef94a5685246d2b85184bc4bf48
rhaarslev <rha@it-minds.dk>
2017-01-02 15:01:21 +0100

    Fixing a bug where the AI calculated the removeCount with m - ak instead of x - ak

--- Ai.fsx
+++ Ai.fsx
@@ -2,34 +2,46 @@ module Ai
     #load "AsyncEventQueue.fsx"
     open AsyncEventQueue
 
-    //
+    (* Recursively xorbs all elements in the list heap
+     * m = a0 ^^^ a1 ^^^ ... ^^^ ax
+     *
+     * fun type: int list -> int
+     *)
     let rec findm heap =
         match heap with
         | [] -> 0
         | x::xs -> x ^^^ findm xs
 
+    (* Recursively traverses the list heap, and looks for a number, where when
+     * xorb'ed with m, it returns a number smaller than the original number.
+     * Returns a tuple of the index and the number.
+     *
+     * fun type: int list -> int -> int * int
+     *)
     let findMove heap m =
         let rec innerFoo heap m idx =
             match heap with
-            | [] -> failwith "Couldn't find k where ak < m"
+            | [] -> failwith "Couldn't find k where ak < m" // Shouldn't ever happen
             | x::xs ->
                 let ak = x ^^^ m
                 in
-                    if ak < x then (idx, m - ak)
-                    else innerFoo xs m (idx+1)
+                    if ak < x then (idx, x - ak) // Found right k
+                    else innerFoo xs m (idx+1) // Try next k
         in
             innerFoo heap m 0
 
+    (* Finds the index of the element in the list with the highest value.
+     *)
     let maxIndexBy list =
         list
         |> List.mapi (fun i x -> i,  x)
         |> List.maxBy snd
         |> fst
 
+    (* Finds the next play-move for the AI
+     *)
     let aiMove heap =
-        printfn "hej"
         let m = findm heap
-        printfn "%A" m
 
         if m = 0 then
             let largestHeapIdx = maxIndexBy heap
--- main.fsx
+++ main.fsx
@@ -9,22 +9,4 @@ open AsyncEventQueue
 open Ai
 open Gui
 
-
-<<<<<<< HEAD
-[<EntryPoint>]
-[<STAThread>]
-let main argv =
-
-    Ai.aiMove [3; 24; 9]
-
-    Application.EnableVisualStyles()
-    Application.SetCompatibleTextRenderingDefault false
-
-
-    Application.Run(Gui.radiobuttonform);
-
-    0
-=======
 Application.Run(Gui.mainWindow);
-
->>>>>>> 9ec2c836f074e5f17ba4ed64adc57ebbe6f6fc83
