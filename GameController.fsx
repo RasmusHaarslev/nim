300b3f8cfa46c4572b90c58842746e3a35e4f86b
rhaarslev <rha@it-minds.dk>
2017-01-03 16:00:45 +0100

    Now uses lists

--- GameController.fsx
+++ GameController.fsx
@@ -1,5 +1,5 @@
 module GameController
-    
+
     #load "AsyncEventQueue.fsx"
     #load "Gui.fsx"
     #load "HeapsZipper.fsx"
@@ -13,37 +13,57 @@ module GameController
     let rand = System.Random()
     type GameState = {
         TurnBit: bool;
-        Heap: HeapsZipper.HeapZipper;
+        Heap: int list;
         AiEnabled: bool
     }
 
     //let mutable gameState = {}
 
-    let move (a,b) gs = { 
-            TurnBit = (not gs.TurnBit);
-            Heap =  (HeapsZipper.subtract a b gs.Heap);
-            AiEnabled = gs.AiEnabled;
+    let rec remove i l =
+        match i, l with
+        | 0, x::xs -> xs
+        | i, x::xs -> x::remove (i - 1) xs
+        | i, [] -> failwith "index out of range"
+
+    let rec subtract i c l =
+        match i, l with
+        | 0, x::xs -> (x - c)::xs
+        | i, x::xs -> x::subtract (i - 1) c xs
+        | i, [] -> failwith "index out of range"
+
+    let move (a,b) gs =
+        let newHeap =
+            if gs.Heap.Item(a) - b <= 0
+            then
+                remove a gs.Heap
+            else
+                subtract a b gs.Heap
+        in {
+            TurnBit = not gs.TurnBit;
+            Heap = newHeap;
+            AiEnabled = gs.AiEnabled
         }
+
 
     let setupNewGame (enableAI) = {
             TurnBit = if rand.Next(0, 1) = 1 then true else false;
-            Heap = HeapsZipper.initFromList [for i in 1 .. rand.Next(3, 15) -> rand.Next(1, 100)];
+            Heap = [for i in 1 .. rand.Next(3, 15) -> rand.Next(1, 100)];
             AiEnabled = enableAI;
         }
 
     let q = AsyncEventQueue.instance
     let rec menu() =
         async {
-            
+
             let! msg = q.Receive()
             match msg with
-                | NewGame   -> 
+                | NewGame   ->
                     let ng = setupNewGame(false)
-                    printfn "%A" (HeapsZipper.toList ng.Heap)
+                    printfn "%A" ng.Heap
                     return! ready(ng)
-                | NewAiGame -> 
+                | NewAiGame ->
                     let ng = setupNewGame(true)
-                    if ng.TurnBit 
+                    if ng.TurnBit
                     then
                         return! aiReady(ng)
                     else
@@ -51,19 +71,19 @@ module GameController
                 | _         -> failwith "Menu: Unexpected Message"
             // Evt make color change to indicate ready.
         }
-    and ready(gameState) = 
+    and ready(gameState) =
         async {
             // Evt make color change to indicate ready.
-            Gui.update (HeapsZipper.toList gameState.Heap)
+            Gui.update gameState.Heap
             let! msg = q.Receive()
             match msg with
-                | Move (a,b)    -> 
+                | Move (a,b)    ->
                     let newGameState = move (a,b) gameState
                     if gameState.AiEnabled then return! aiReady(newGameState)
                     else return! ready(newGameState)
                 | Clear         ->
                     return! menu()
-                | NewGame         -> return! ready(setupNewGame(false))  
+                | NewGame         -> return! ready(setupNewGame(false))
                 | NewAiGame       -> return! ready(setupNewGame(true))
                 | _                         -> failwith "Ready: Unexpected Message"
         }
@@ -71,9 +91,9 @@ module GameController
         async {
             // Evt make color change to indicate ready.
             printfn "IN AI MAKE MOVE"
-            Gui.update (HeapsZipper.toList gameState.Heap)
+            Gui.update gameState.Heap
 
-            let moveTupl = Ai.aiMove (HeapsZipper.toList gameState.Heap)
+            let moveTupl = Ai.aiMove gameState.Heap
             printfn "AiMove: %A" moveTupl
             let ns = move moveTupl gameState
             return! ready(ns)
