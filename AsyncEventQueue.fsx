f3d2438e013558fe7cb17dfa430ec6abc34a097a
Gustav Jonsson <gustavj.dotzorx@gmail.com>
2017-01-04 13:07:01 +0100

    Changed eventQ to use new type file.

--- AsyncEventQueue.fsx
+++ AsyncEventQueue.fsx
@@ -1,4 +1,9 @@
 module AsyncEventQueue
+    
+    #load "types.fsx"
+    open Types
+
+
     open System
     open System.Threading
 
@@ -25,14 +30,6 @@ module AsyncEventQueue
                 tryListen cont)
 
     // An enumeration of the possible events
-    type Message =
-        | Move of int*int
-        | Error
-        | SelectHeap of int
-        | NewGame
-        | NewAiGame 
-        | Clear
-        | Download of string
     // Defining an insatnce here works, but we should investigate if there's
     // a cool way of doing singletons!!
-    let (instance: AsyncEventQueue<Message>) = AsyncEventQueue()
+    let (instance: AsyncEventQueue<Types.Message>) = AsyncEventQueue()
