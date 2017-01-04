8c2935c74cd3dd21e76ffde75c6aa224e82b1466
Gustav Jonsson <gustavj.dotzorx@gmail.com>
2017-01-04 13:03:30 +0100

    Adding types.fsx file

---
+++ types.fsx
@@ -0,0 +1,23 @@
+module Types
+    
+    type AiType =
+        | None
+        | Easy
+        | Medium
+        | Hard
+        | Godlike
+        
+    type Message =
+        | Move of int*int
+        | Error
+        | SelectHeap of int
+        | NewGame
+        | NewAiGame 
+        | Clear
+        | Download of string
+
+    type GameState = {
+        TurnBit: bool;
+        Heap: int list;
+        Ai: AiType
+    }
